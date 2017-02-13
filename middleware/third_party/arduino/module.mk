16e9c1a262733437c99c01902a523174bbfa1c8d
Pablo Sun <pablo.sun@mediatek.com>
2017-02-13 17:10:27 +0800

    Revise Arduino middleware makefile
    Instead of listing soruce files in module.mk, we should enlist them in Makefile. In project's Makefile, we remove the redundant include path settings, and simply include module.mk.

--- middleware/third_party/arduino/module.mk
+++ middleware/third_party/arduino/module.mk
@@ -6,32 +6,6 @@ ARDUINO_CORE = hardware/arduino/mt7697/cores/arduino
 ARDUINO_VARS = hardware/arduino/mt7697/variants/$(BOARD_CONFIG)
 ARDUINO_LIBS = hardware/arduino/mt7697/libraries
 
-S-FILES      = $(wildcard $(ARDUINO_VARS)/*.s)
-
-C-FILES      = $(wildcard $(ARDUINO_CORE)/*.c)
-C-FILES     += $(wildcard $(ARDUINO_VARS)/*.c)
-C-FILES     += $(wildcard $(ARDUINO_LIBS)/Wire/src/*.c)
-C-FILES     += $(wildcard $(ARDUINO_LIBS)/MCS/src/*.c)
-C-FILES     += $(wildcard $(ARDUINO_LIBS)/LTimer/src/*.c)
-C-FILES     += $(wildcard $(ARDUINO_LIBS)/LFlash/src/*.c)
-C-FILES     += $(wildcard $(ARDUINO_LIBS)/SPI/src/*.c)
-C-FILES     += $(wildcard $(ARDUINO_LIBS)/LWiFi/src/*.c)
-C-FILES     += $(wildcard $(ARDUINO_LIBS)/LWiFi/src/utility/*.c)
-
-CXX-FILES    = $(wildcard $(ARDUINO_CORE)/*.cpp)
-CXX-FILES   += $(wildcard $(ARDUINO_VARS)/*.cpp)
-CXX-FILES   += $(wildcard $(ARDUINO_LIBS)/Wire/src/*.cpp)
-CXX-FILES   += $(wildcard $(ARDUINO_LIBS)/MCS/src/*.cpp)
-CXX-FILES   += $(wildcard $(ARDUINO_LIBS)/LTimer/src/*.cpp)
-CXX-FILES   += $(wildcard $(ARDUINO_LIBS)/LFlash/src/*.cpp)
-CXX-FILES   += $(wildcard $(ARDUINO_LIBS)/SPI/src/*.cpp)
-CXX-FILES   += $(wildcard $(ARDUINO_LIBS)/LWiFi/src/*.cpp)
-CXX-FILES   += $(wildcard $(ARDUINO_LIBS)/LWiFi/src/utility/*.cpp)
-
-S_FILES      = $(addprefix middleware/third_party/arduino/,$(S-FILES))
-C_FILES      = $(addprefix middleware/third_party/arduino/,$(C-FILES))
-CXX_FILES    = $(addprefix middleware/third_party/arduino/,$(CXX-FILES))
-
 ###################################################
 # include path
 INC_FLAGS += -I$(SOURCE_DIR)/$(ARDUINO_SRC)/$(ARDUINO_CORE)
