795216c71acdca9f7c9962843a5f6839ff392788
Pablo Sun <pablo.sun@mediatek.com>
2017-02-13 18:04:16 +0800

    Remove mt7687_hdk project files

--- project/mt7687_hdk/apps/arduino/flash/GCC/feature.mk
+++
@@ -1,4 +0,0 @@
-IC_CONFIG                           = mt7687
-BOARD_CONFIG                        = mt7687_hdk
-MTK_FW_VERSION                      = mt7687_fw
-MTK_NVDM_ENABLE                     = y
