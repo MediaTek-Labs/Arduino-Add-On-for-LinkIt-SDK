ad545be2eba4e6d41421158fd459801a518d0166
Pablo Sun <pablo.sun@mediatek.com>
2017-02-16 17:42:17 +0800

    Update WiFiClient Example
    Download ascii text instead.

--- middleware/third_party/arduino/hardware/arduino/mt7697/libraries/LWiFi/examples/WiFiWebClient/WiFiWebClient.ino
+++ middleware/third_party/arduino/hardware/arduino/mt7697/libraries/LWiFi/examples/WiFiWebClient/WiFiWebClient.ino
@@ -1,33 +1,34 @@
 /*
   Web client
 
-  This sketch connects to a website (http://www.google.com)
-  using a WiFi shield.
-
-  This example is written for a network using WPA encryption. For
-  WEP or WPA, change the Wifi.begin() call accordingly.
+  This sketch connects to a website (http://download.labs.mediatek.com)
+  using LinkIt 7697
 
   This example is written for a network using WPA encryption. For
   WEP or WPA, change the Wifi.begin() call accordingly.
 
   Circuit:
-  * WiFi shield attached
+  * LinkIt 7697
 
   created 13 July 2010
   by dlf (Metodo2 srl)
   modified 31 May 2012
   by Tom Igoe
+  modified Jan 2017
+  by MediaTek Labs
 */
 
 #include <LWiFi.h>
 
-int keyIndex = 0;            // your network key Index number (needed only for WEP)
+char ssid[] = "your_ssid";      //  your network SSID (name)
+char pass[] = "your_password";  // your network password (use for WPA, or use as key for WEP)
+int keyIndex = 0;               // your network key Index number (needed only for WEP)
 
 int status = WL_IDLE_STATUS;
 // if you don't want to use DNS (and reduce your sketch size)
 // use the numeric IP instead of the name for the server:
-//IPAddress server(117,185,24,248);  //
-char server[] = "apis.map.qq.com";   //
+//IPAddress server(117,185,24,248);
+char server[] = "download.labs.mediatek.com";   // http://download.labs.mediatek.com/linkit_7697_ascii.txt
 
 // Initialize the Ethernet client library
 // with the IP address and port of the server
@@ -35,81 +36,84 @@ char server[] = "apis.map.qq.com";   //
 WiFiClient client;
 
 void setup() {
-	//Initialize serial and wait for port to open:
-	Serial.begin(9600);
-	while (!Serial) {
-		; // wait for serial port to connect. Needed for native USB port only
-	}
+    //Initialize serial and wait for port to open:
+    Serial.begin(9600);
+    while (!Serial) {
+        ; // wait for serial port to connect. Needed for native USB port only
+    }
 
-	// check for the presence of the shield:
-	if (WiFi.status() == WL_NO_SHIELD) {
-		Serial.println("WiFi shield not present");
-		// don't continue:
-		while (true);
-	}
+    // check for the presence of the shield:
+    if (WiFi.status() == WL_NO_SHIELD) {
+        Serial.println("WiFi shield not present");
+        // don't continue:
+        while (true);
+    }
 
-	String fv = WiFi.firmwareVersion();
-	if (fv != "1.1.0") {
-		Serial.println("Please upgrade the firmware");
-	}
+    String fv = WiFi.firmwareVersion();
+    if (fv != "1.1.0") {
+        Serial.println("Please upgrade the firmware");
+    }
 
-	// attempt to connect to Wifi network:
-	while (status != WL_CONNECTED) {
-		Serial.print("Attempting to connect to SSID: ");
-		Serial.println(ssid);
-		// Connect to WPA/WPA2 network. Change this line if using open or WEP network:
-		status = WiFi.begin(ssid, pass);
+    // attempt to connect to Wifi network:
+    while (status != WL_CONNECTED) {
+        Serial.print("Attempting to connect to SSID: ");
+        Serial.println(ssid);
+        // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
+        status = WiFi.begin(ssid, pass);
 
-		// wait 10 seconds for connection:
-		delay(10000);
-	}
-	Serial.println("Connected to wifi");
-	printWifiStatus();
+        // wait 10 seconds for connection:
+        delay(10000);
+    }
+    Serial.println("Connected to wifi");
+    printWifiStatus();
 
-	Serial.println("\nStarting connection to server...");
-	// if you get a connection, report back via serial:
-	if (client.connect(server, 80)) {
-		Serial.println("connected to server (GET)");
-		// Make a HTTP request:
-		client.println("GET /ws/location/v1/ip?ip=61.135.17.68&key=6MABZ-VFKAF-DITJ6-JRPZN-OUOFJ-ULBWQ HTTP/1.1");
-		client.println("Host: apis.map.qq.com");
-		client.println();
-	}
+    Serial.println("\nStarting connection to server...");
+    // if you get a connection, report back via serial:
+    if (client.connect(server, 80)) {
+        Serial.println("connected to server (GET)");
+        // Make a HTTP request:
+        client.println("GET /linkit_7697_ascii.txt HTTP/1.0");
+        client.println("Host: download.labs.mediatek.com");
+        client.println("Accept: */*");
+        client.println("Connection: close");
+        client.println();
+        delay(10);
+    }
 }
 
 void loop() {
-	// if there are incoming bytes available
-	// from the server, read them and print them:
-	while (client.available()) {
-		char c = client.read();
-		Serial.write(c);
-	}
+    // if there are incoming bytes available
+    // from the server, read them and print them:
+    while (client.available()) {
+        char c = client.read();
+        Serial.write(c);
+    }
 
-	// if the server's disconnected, stop the client:
-	if (!client.connected()) {
-		Serial.println();
-		Serial.println("disconnecting from server.");
-		client.stop();
+    // if the server's disconnected, stop the client:
+    if (!client.connected()) {
+        Serial.println();
+        Serial.println("disconnecting from server.");
+        client.stop();
 
-		// do nothing forevermore:
-		while (true);
-	}
+        // do nothing forevermore:
+        while (true);
+    }
 }
 
 
 void printWifiStatus() {
-	// print the SSID of the network you're attached to:
-	Serial.print("SSID: ");
-	Serial.println(WiFi.SSID());
+    // print the SSID of the network you're attached to:
+    Serial.print("SSID: ");
+    Serial.println(WiFi.SSID());
 
-	// print your WiFi shield's IP address:
-	IPAddress ip = WiFi.localIP();
-	Serial.print("IP Address: ");
-	Serial.println(ip);
+    // print your WiFi shield's IP address:
+    IPAddress ip = WiFi.localIP();
+    Serial.print("IP Address: ");
+    Serial.println(ip);
 
-	// print the received signal strength:
-	long rssi = WiFi.RSSI();
-	Serial.print("signal strength (RSSI):");
-	Serial.print(rssi);
-	Serial.println(" dBm");
+    // print the received signal strength:
+    long rssi = WiFi.RSSI();
+    Serial.print("signal strength (RSSI):");
+    Serial.print(rssi);
+    Serial.println(" dBm");
 }
