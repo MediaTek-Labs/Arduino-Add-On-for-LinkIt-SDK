4523515b168ed1e3e4be9d4fe9f5a3fcb85ee5f4
Pablo Sun <pablo.sun@mediatek.com>
2017-03-03 18:25:06 +0800

    Add BLE begin example

--- middleware/third_party/arduino/hardware/arduino/mt7697/libraries/LBLE/examples/ScanPeripherals/ScanPeripherals.ino
+++ middleware/third_party/arduino/hardware/arduino/mt7697/libraries/LBLE/examples/ScanPeripherals/ScanPeripherals.ino
@@ -13,9 +13,8 @@
 void setup() {
 	//Initialize serial and wait for port to open:
 	Serial.begin(9600);
-	while (!Serial) {
-		; // wait for serial port to connect. Needed for native USB port only
-	}
+
+  Serial.println("start init BLE");
 
 	// Initialize BLE subsystem
 	LBLE.begin();
@@ -24,6 +23,7 @@ void setup() {
 		delay(100);
 	}
 
+  Serial.println("BLE init done");
 	// scanner.startScan();
 }
 
