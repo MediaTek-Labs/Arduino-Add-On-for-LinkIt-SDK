75ff79f7a16ce2d8564c7290ffb29da6613e77e5
Pablo Sun <pablo.sun@mediatek.com>
2017-03-20 16:36:24 +0800

    Follow Arduino's style guide on INO files

--- middleware/third_party/arduino/hardware/arduino/mt7697/libraries/LBLE/examples/ScanPeripherals/ScanPeripherals.ino
+++ middleware/third_party/arduino/hardware/arduino/mt7697/libraries/LBLE/examples/ScanPeripherals/ScanPeripherals.ino
@@ -8,76 +8,69 @@
 #include <LBLECentral.h>
 
 LBLECentral scanner;
-	
+
 void setup() {
-	//Initialize serial
-	Serial.begin(9600);
+  //Initialize serial
+  Serial.begin(9600);
 
-	// Initialize BLE subsystem
-	Serial.println("BLE begin");
-	LBLE.begin();
-	while(!LBLE.ready())
-	{
-		delay(10);
-	}
-	Serial.println("BLE ready");
+  // Initialize BLE subsystem
+  Serial.println("BLE begin");
+  LBLE.begin();
+  while (!LBLE.ready()) {
+    delay(10);
+  }
+  Serial.println("BLE ready");
 
-	// start scanning nearby advertisements
-	scanner.scan();
+  // start scanning nearby advertisements
+  scanner.scan();
 }
 
-void printDeviceInfo(int i)
-{
-	Serial.print("Addr: ");
-	Serial.println(scanner.getAddress(i));
-	Serial.print("RSSI: ");
-	Serial.println(scanner.getRSSI(i));
-	Serial.print("Name: ");
-	Serial.println(scanner.getName(i));
-	Serial.print("UUID: ");
-	if(!scanner.getServiceUuid(i).isEmpty())
-	{
-		Serial.println(scanner.getServiceUuid(i));
-	}
-	else
-	{
-		Serial.println();
-	}
-	Serial.print("Flag: ");
-	Serial.println(scanner.getAdvertisementFlag(i), HEX);
-	Serial.print("Manu: ");
-	Serial.println(scanner.getManufacturer(i));
+void printDeviceInfo(int i) {
+  Serial.print("Addr: ");
+  Serial.println(scanner.getAddress(i));
+  Serial.print("RSSI: ");
+  Serial.println(scanner.getRSSI(i));
+  Serial.print("Name: ");
+  Serial.println(scanner.getName(i));
+  Serial.print("UUID: ");
+  if (!scanner.getServiceUuid(i).isEmpty()) {
+    Serial.println(scanner.getServiceUuid(i));
+  } else {
+    Serial.println();
+  }
+  Serial.print("Flag: ");
+  Serial.println(scanner.getAdvertisementFlag(i), HEX);
+  Serial.print("Manu: ");
+  Serial.println(scanner.getManufacturer(i));
 
-	if(scanner.isIBeacon(i))
-	{
-		LBLEUuid uuid;
-		uint16_t major = 0, minor = 0;
-		uint8_t txPower = 0;
-		scanner.getIBeaconInfo(i, uuid, major, minor, txPower);
+  if (scanner.isIBeacon(i)) {
+    LBLEUuid uuid;
+    uint16_t major = 0, minor = 0;
+    uint8_t txPower = 0;
+    scanner.getIBeaconInfo(i, uuid, major, minor, txPower);
 
-		Serial.println("iBeacon->");
-		Serial.print("    UUID: ");
-		Serial.println(uuid);
-		Serial.print("    Major: ");
-		Serial.println(major);
-		Serial.print("    Minor: ");
-		Serial.println(minor);
-		Serial.print("    txPower: ");
-		Serial.println(txPower);
-	}
+    Serial.println("iBeacon->");
+    Serial.print("    UUID: ");
+    Serial.println(uuid);
+    Serial.print("    Major: ");
+    Serial.println(major);
+    Serial.print("    Minor: ");
+    Serial.println(minor);
+    Serial.print("    txPower: ");
+    Serial.println(txPower);
+  }
 }
 
 void loop() {
-	// wait for a while
-	delay(3000);
+  // wait for a while
+  delay(3000);
 
-	// enumerate advertisements found.
-	Serial.println(scanner.getPeripheralCount());
-	for(int i = 0; i < scanner.getPeripheralCount(); ++i)
-	{
-		Serial.println("---");
-		printDeviceInfo(i);
-	}
-	
-	Serial.println("----------------------");
+  // enumerate advertisements found.
+  Serial.println(scanner.getPeripheralCount());
+  for (int i = 0; i < scanner.getPeripheralCount(); ++i) {
+    Serial.println("---");
+    printDeviceInfo(i);
+  }
+
+  Serial.println("----------------------");
 }
--- middleware/third_party/arduino/hardware/arduino/mt7697/libraries/LBLE/examples/SimplePeripheral/SimplePeripheral.ino
+++ middleware/third_party/arduino/hardware/arduino/mt7697/libraries/LBLE/examples/SimplePeripheral/SimplePeripheral.ino
@@ -1,79 +1,72 @@
 /*
-  This example configures LinkIt 7697 to act as a simple GATT server with 1 custom +attribute.
+  This example configures LinkIt 7697 to act as a simple GATT server with 1 characteristic.
 
   created Mar 2017
 */
 #include <LBLE.h>
 #include <LBLEPeriphral.h>
 
+// Define a simple GATT service with only 1 characteristic
 LBLEService ledService("19B10010-E8F2-537E-4F6C-D104768A1214");
 LBLECharacteristicInt switchCharacteristic("19B10011-E8F2-537E-4F6C-D104768A1214", LBLE_READ | LBLE_WRITE);
 
 void setup() {
-	//Initialize serial and wait for port to open:
-	Serial.begin(9600);
 
-	// Initialize BLE subsystem
-	Serial.println("BLE begin");
-	LBLE.begin();
-	while(!LBLE.ready())
-	{
-		delay(100);
-	}
-	Serial.println("BLE ready");
+  // Initialize LED pin
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
 
-	// TODO: insert a simple GATT service with only 1 attribute
+  //Initialize serial and wait for port to open:
+  Serial.begin(9600);
 
-	// configure our advertisement data.
-	// In this case, we simply create an advertisement that represents an
-	// connectable device with a device name "Simple BLE".
-	LBLEAdvertisementData advertisement;
-	advertisement.configAsConnectableDevice("UNIQLO");
+  // Initialize BLE subsystem
+  Serial.println("BLE begin");
+  LBLE.begin();
+  while (!LBLE.ready()) {
+    delay(100);
+  }
+  Serial.println("BLE ready");
 
-	// configure our device's Generic Access Profile's device name
-	// TODO: This is currently not working.
-	LBLEPeripheral.setName("LinkIt Simple BLE");
+  Serial.print("device address is:");
+  Serial.println(LBLE.getDeviceAddress());
 
-	ledService.addAttribute(switchCharacteristic);
-	LBLEPeripheral.addService(ledService);
+  // configure our advertisement data.
+  // In this case, we simply create an advertisement that represents an
+  // connectable device with a device name
+  LBLEAdvertisementData advertisement;
+  advertisement.configAsConnectableDevice("BLE LED");
 
-	// start the GATT server
-	LBLEPeripheral.begin();
+  // Configure our device's Generic Access Profile's device name
+  // Ususally this is the same as the name in the advertisement data.
+  LBLEPeripheral.setName("BLE LED");
 
-	// start advertisment
-	LBLEPeripheral.advertise(advertisement);
+  // Add characteristics into ledService
+  ledService.addAttribute(switchCharacteristic);
 
-	// Initialize GPIO
-	pinMode(LED_BUILTIN, OUTPUT);
+  // Add service to GATT server (peripheral)
+  LBLEPeripheral.addService(ledService);
 
+  // start the GATT server - it is now 
+  // available to connect
+  LBLEPeripheral.begin();
+
+  // start advertisment
+  LBLEPeripheral.advertise(advertisement);
 }
 
 void loop() {
-	// The underlying framework will advertise periodically.
-	// we simply wait here.
-	// 
-	// You can use iOS/Android apps such as
-	// "BLE Finder" to scan this peripheral and check the
-	// services it provides.
-	delay(200);
-
-	if(switchCharacteristic.isWritten())
-	{
-		const char value = switchCharacteristic.getValue();
-		Serial.print("sketch->");
-		Serial.println(value);
-		switch(value)
-		{
-		case 'O':
-			digitalWrite(LED_BUILTIN, HIGH);
-			break;
-		case 'C':
-			digitalWrite(LED_BUILTIN, LOW);
-			break;
-		default:
-			Serial.print("not O, C->");
-			Serial.println(value);
-			break;
-		}
-	}
+  delay(100);
+  if (switchCharacteristic.isWritten()) {
+    const char value = switchCharacteristic.getValue();
+    switch (value) {
+      case 'O':
+        digitalWrite(LED_BUILTIN, HIGH);
+        break;
+      case 'C':
+        digitalWrite(LED_BUILTIN, LOW);
+        break;
+      default:
+        break;
+    }
+  }
 }
