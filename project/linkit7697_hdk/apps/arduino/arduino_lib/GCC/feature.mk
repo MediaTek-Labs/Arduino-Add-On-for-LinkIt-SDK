1d3ca40c11449a16a0e9256714b7dda84549bef9
pablo.sun <pablo.sun@mediatek.com>
2017-06-14 15:44:43 +0800

    Update board config name to linkit7697_hdk in feature.mk

--- project/linkit7697_hdk/apps/arduino/arduino_lib/GCC/feature.mk
+++ project/linkit7697_hdk/apps/arduino/arduino_lib/GCC/feature.mk
@@ -1,5 +1,5 @@
 IC_CONFIG                           = mt7697
-BOARD_CONFIG                        = linkit_7697
+BOARD_CONFIG                        = linkit7697_hdk
 MTK_FW_VERSION                      = mt7697_fw_
 MTK_NVDM_ENABLE                     = y
 MTK_WIFI_WPS_ENABLE                 = y
