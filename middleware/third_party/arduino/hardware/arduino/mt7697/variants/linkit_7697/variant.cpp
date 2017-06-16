e207e704af6f13b42b2af67ed7bdee1ad6cf892f
pablo.sun <pablo.sun@mediatek.com>
2017-06-16 11:10:37 +0800

    Increase arduino task stack size
    The task usage increased around 880 bytes when migrating from v4.2 to v4.3. So we increase the stack size for 1024 word to 2048 word.

--- middleware/third_party/arduino/hardware/arduino/mt7697/variants/linkit_7697/variant.cpp
+++ middleware/third_party/arduino/hardware/arduino/mt7697/variants/linkit_7697/variant.cpp
@@ -12,13 +12,34 @@ extern "C" {
 #include <task.h>
 #endif
 
+static void debug_task_stack_usage(void)
+{
+#if 0
+	if(xTaskGetCurrentTaskHandle())
+	{
+		static UBaseType_t maxStackUsage = 0;
+		const UBaseType_t stackUsageWord = uxTaskGetStackHighWaterMark(xTaskGetCurrentTaskHandle());
+		if(stackUsageWord > maxStackUsage)
+		{
+			maxStackUsage = stackUsageWord;
+			Serial.print("remaining stack quota=");
+			// convert word to byte
+			Serial.println(maxStackUsage * 4);
+		}
+	}
+#endif
+}
+
 static void arduino_task(void *args)
 {
 	setup();
+	debug_task_stack_usage();
 
 	while (1) {
 		loop();
 		if (serialEventRun) serialEventRun();
+
+		debug_task_stack_usage();	
 	}
 	return ;
 }
@@ -29,7 +50,7 @@ void init(void)
 
 #ifdef FREERTOS
 	// Create a Task to run arduino application
-	xTaskCreate(arduino_task, "arduino_task", 1024, NULL, 1, NULL);
+	xTaskCreate(arduino_task, "arduino_task", 2048, NULL, 1, NULL);
 #endif
 }
 
