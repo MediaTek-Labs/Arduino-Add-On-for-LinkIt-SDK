ca5c32612848c36a49b71f3c463724dead9eaade
Pablo Sun <pablo.sun@mediatek.com>
2017-08-30 17:26:31 +0800

    Add new round, big button example

--- middleware/third_party/arduino/hardware/arduino/mt7697/libraries/LRemote/examples/RemoteControl/RemoteControl.ino
+++ middleware/third_party/arduino/hardware/arduino/mt7697/libraries/LRemote/examples/RemoteControl/RemoteControl.ino
@@ -10,7 +10,7 @@ LRemoteUIControl btn;
 LRemoteUIControl slider;
 LRemoteUIControl label;
 LRemoteUIControl switchButton;
-
+LRemoteUIControl bigButton;
 
 void setup() {
   //Initialize serial and wait for port to open:
@@ -22,9 +22,11 @@ void setup() {
     delay(100);
   }
 
+  // Setup the Remote Control's UI canvas
   LRemote.setGrid(3, 5);
   LRemote.setName("Demo Con");
 
+  // Add a push button
   btn.setType(RC_PUSHBUTTON);
   btn.setText("OK");
   btn.setPos(1, 1);
@@ -32,6 +34,15 @@ void setup() {
   btn.setColor(RC_PINK);
   LRemote.addControl(btn);
 
+  // Add a big, round button
+  bigButton.setType(RC_CIRCLEBUTTON);
+  bigButton.setText("OK");
+  bigButton.setPos(0, 3);
+  bigButton.setSize(3, 2);
+  bigButton.setColor(RC_GREEN);
+  LRemote.addControl(bigButton);
+
+  // Add a slider
   slider.setType(RC_SLIDER);
   slider.setText("MinMax");
   slider.setPos(0, 2);
@@ -39,6 +50,7 @@ void setup() {
   slider.setColor(RC_GOLD);
   LRemote.addControl(slider);
 
+  // Add a simple text label
   label.setType(RC_LABEL);
   label.setText("Remote Test");
   label.setPos(0, 0);
@@ -46,13 +58,15 @@ void setup() {
   label.setColor(RC_GREY);
   LRemote.addControl(label);
 
+  // Add an on/off switch
   switchButton.setType(RC_SWITCHBUTTON);
   switchButton.setText("Power");
   switchButton.setPos(0, 1);
   switchButton.setSize(1, 1);
-  switchButton.setColor(RC_GREEN);
+  switchButton.setColor(RC_BLUE);
   LRemote.addControl(switchButton);
 
+  // Start broadcasting our remote contoller
   LRemote.begin();
 }
 
@@ -65,11 +79,27 @@ void loop() {
     delay(100);
   }
   
+  // Process the incoming BLE write request
+  // and translate them to control events
   LRemote.process();
 
+  // Now we poll each control's status
+  
   if(btn.hasEvent()){
     RCEventInfo info = btn.getLastEvent();
     Serial.print("btn event=");
+    switch(info.event) {
+      case RC_BTNUP:
+        Serial.println("BTN_UP");
+        break;
+      case RC_BTNDOWN:
+        Serial.println("BTN_DOWN");
+    }
+  }
+
+  if(bigButton.hasEvent()){
+    RCEventInfo info = bigButton.getLastEvent();
+    Serial.print("bigButton event=");
     switch(info.event) {
       case RC_BTNUP:
         Serial.println("BTN_UP");
