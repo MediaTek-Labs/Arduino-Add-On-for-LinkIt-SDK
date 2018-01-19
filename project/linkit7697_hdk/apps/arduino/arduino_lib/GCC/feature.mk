5904bdd26ed7749dc2d3508689800a054d727b94
pablo.sun <pablo.sun@mediatek.com>
2018-01-19 14:05:35 +0800

    Turn off WPS, which is not used

--- project/linkit7697_hdk/apps/arduino/arduino_lib/GCC/feature.mk
+++ project/linkit7697_hdk/apps/arduino/arduino_lib/GCC/feature.mk
@@ -2,7 +2,6 @@ IC_CONFIG                           = mt7697
 BOARD_CONFIG                        = linkit7697_hdk
 MTK_FW_VERSION                      = mt7697_fw_
 MTK_NVDM_ENABLE                     = y
-MTK_WIFI_WPS_ENABLE                 = y
 MTK_WIFI_REPEATER_ENABLE            = y
 MTK_MINISUPP_ENABLE                 = y
 MTK_BT_ENABLE                       = y
